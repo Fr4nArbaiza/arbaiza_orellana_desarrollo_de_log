c451e26acf617e42ad9cf0d089847c19647735de
Fr4nArbaiza <franlex.arbaiza@gmail.com>
2025-05-07 20:05:20 -0600

    INICIO DE SESION PARTE 1

---
+++ core/secure/ips.php
@@ -0,0 +1,9 @@
+<?php
+$rango = array(
+    "127.0.0.1/32",                 // PERMITE UNICAMENTE LA DIRECCION LOCAL
+    "192.168.1.0/24",               // PERMITE TODOS LOS HOST DE LA RED 192.168.1.*
+    "10.0.0.*",                     // PERMITE TODOS LOS HOST DE LA RED 10.0.0.*
+    "172.16.10.1-172.16.10.50"      // PERMITE RANGO DE IP ESPECÍFICO
+)
+
+?>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+ini_set("display_errors",'1');
+ini_set("display_startup_errors",'1');
+error_reporting(E_ALL);
+include("core/inc/funciones.inc.php");
+include("core/secure/ips.php");
+$archivo = "./logs/logs.log";
+$ip = ip_in_ranges($_SERVER["REMOTE_ADDR"], $rango);
+
+?>
+<!DOCTYPE html>
+<html lang="es-SV">
+    <head>
+        <title>Inicio de sesión: Francisco Alexander Arbaiza Orellana</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" href="css/bootstrap.css" />
+        <link href=fonts/fontawesome/css/all.css" rel="stylesheet" />
+        <script type="text-javascript" src="js/jquery-3.7.1.min.js"></script>
+        <script type="text-javascript" src="js/bootstrap.js"></script>
+        <script type="text-javascript" src="js/sweetalert.js"></script>
+        <script type="text-javascript" src="fonts/fontawesome/js/all.js"></script>
+    </head>
+    <body class="bg-light">
+        <div class="alert alert-warning" role="alert">
+
+        <b>Bienvenido!</b> Iniciar Sesión Para Continuar
+
+        </div>
+        <div class="form-row">
+        <div class="form-group col-md-5 text-center">
+            <img src="media/logo/logo_corporativo.png" class="mx-auto d-block" id="img" width="50%" height="auto"/>
+        </div>
+
+        <div class="form-group col-md-5 ml-4 mr-4 justify-content-center align-self-center">
+            <h1> Diseñando Estrategias para la Recuperación y Migración de Base de Datos <strong>(RBK0)</strong></h1>
+            <form name="frm_iniciar_sesion" id="frm_iniciar_sesion" action="core/process.php" method="post">
+                <div class="form-group">
+                    <label for="txt_user">Usurio:<br></label>
+                    <input type="text" class="form-control" id="txt_user" name="txt_user" aria-describedby="txt_userHelp" maxlength="10" required>
+                    <small id="txt_userHelp" class="form-text text-muted">Diguite un usuario <i class="text-danger">(* Campo Obligatorio)</i></small>
+                </div>
+
+                <div class="form-group">
+                    <label for="text_pass">Contraseña</label>
+                    <input type="password" class="form-control" id="txt_pass" name="txt_pass" aria-describedby="txt_passHelp" placeholder="Ingrese su contraseña" maxlength="10" required>
+                    <small id="txt_passHelp" class="form-text text-muted">Diguite su contraseña <i class="text-danger">(* Campo Obligatorio)</i></small>
+                </div>
+                <button type="submit" id="btn_ingresar" class="btn btn-primary mx-auto d-block" value="ingresar"> Iniciar Sesión </button>
+            </form>
+        </div>
+        </div>
+        
+        
+            
+    </body>
+</html>
